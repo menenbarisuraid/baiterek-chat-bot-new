76a538db276dbe334b81bca1f61cb8640567cf9f
olzhas.sadan <olzhas.sadan@qcloudy.io>
2025-06-13 11:54:40 +0500

    corrected

--- src/App.tsx
+++ src/App.tsx
@@ -42,9 +42,9 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
-import outputs from "../amplify_outputs.json";
+// import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
 
 export default function App() {
   return (
