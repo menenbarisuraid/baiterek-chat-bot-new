0043342a1b0a439f5841aa087389f1a6af7a3210
olzhas.sadan <olzhas.sadan@qcloudy.io>
2025-06-13 11:43:11 +0500

    test cognito

---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Amplify } from "aws-amplify";
+import outputs from "../amplify_outputs.json";
+
+// Amplify.configure(outputs);
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "eu-central-1_xaCLPHhTs",
+      userPoolClientId: "5plk66v79glgvfptci5mer5ne5",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+});
+
